e5512133ce4b141402fcec8df7840c00ae1206e1
Luiz Gustavo <cavalcantelg7@gmail.com>
2026-08-23 22:09:30 -0300

    feat: create stg_price_history.sql

---
+++ crypto_pipeline/models/staging/stg_price_history.sql
@@ -0,0 +1,18 @@
+with source as (
+    select * from {{ source('raw', 'price_history') }}
+),
+
+final as (
+    select
+        coin_id,
+        TO_TIMESTAMP_NTZ(f.value[0]::NUMBER, 3) as date_timestamp,
+        round(f.value[1],2) as price_value,
+        round(market_caps[f.index][1],2) as market_caps_value,
+        round(total_volumes[f.index][1],2) as total_volume_value,
+        current_timestamp() as loaded_at
+    from source,
+    lateral flatten(input => prices) f
+)
+
+select *
+from final
